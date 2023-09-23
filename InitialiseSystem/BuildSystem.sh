943c480d4c21c3e44e71bc4e7607f8b915d778ea
Katan Patel <katan.sap.development@gmail.com>
2023-09-23 14:13:41 +1000

    trying as su -l username

--- InitialiseSystem/BuildSystem.sh
+++ InitialiseSystem/BuildSystem.sh
@@ -38,18 +38,14 @@ else
 fi
 
 # 1. Lock Root User
-shout "Switching user to $systemUser"
-su -l $systemUser
-try echo "User switched to $(whoami)"  
-
 shout "Locking root password to disable root login via password" 
-try passwd -l root
+try su -l $systemUser -c 'passwd -l root'
 
 # 2. SSH Setup
 read -r -p "Do you wish to setup an SSH key (y/n)? " createSSHKey
 if [ "$createSSHKey" = "y" ]; then
     shout "Creating SSH Key" 
-    ssh-keygen -t ed25519 -f id_rsa
+    try su -l $systemUser -c 'ssh-keygen -t ed25519 -f id_rsa'
     shout "Copying keys to /home/$systemUser/.ssh/authorized_keys" 
     shout "Current Directory is: " && pwd 
     mkdir -p "/home/$systemUser/.ssh"
@@ -58,9 +54,9 @@ if [ "$createSSHKey" = "y" ]; then
     else
         die "Directory /home/$systemUser/.ssh was not successfully created" 
     fi
-    touch "/home/$systemUser/.ssh/authorized_keys"
-    chmod -R go= "/home/$systemUser/.ssh"
-    cat id_rsa.pub >>"/home/$systemUser/.ssh/authorized_keys"    
+    try su -l $systemUser -c 'touch "/home/$systemUser/.ssh/authorized_keys"'
+    try su -l $systemUser -c 'chmod -R go= "/home/$systemUser/.ssh"'
+    try su -l $systemUser -c 'cat id_rsa.pub >>"/home/$systemUser/.ssh/authorized_keys"'
 fi
 if [ "$createSSHKey" = "n" ]; then
     shout "You can manually copy generated ssh keys to /home/$systemUser/.ssh/authorized_keys if required"
@@ -69,52 +65,53 @@ fi
 read -r -p "Enter new SSH Port (ensure cloud provider firewall has this port open): " sshPort
 
 shout "Update SSH login to disable root login"
-echo "
+# Could these be added to the sshd_config.d override file instead???
+try su -l $systemUser -c  'echo "
 Port $sshPort
 PasswordAuthentication no
 PermitRootLogin no
 AllowUsers $systemUser
-" >>/etc/ssh/sshd_config # Could these be added to the sshd_config.d override file instead
+" >>/etc/ssh/sshd_config' 
 
-chmod -R go= ~/.ssh
-chown -R "$systemUser:$systemUser" ~/.ssh
+try su -l $systemUser -c 'chmod -R go= ~/.ssh'
+try su -l $systemUser -c 'chown -R "$systemUser:$systemUser" ~/.ssh'
 
-systemctl restart sshd
+try su -l $systemUser -c  'systemctl restart sshd'
 shout "Created SSH Key and copied to /.ssh/authorized_keys"
 
 # 3. Firewall Setup
 shout "Configuring Ports"
-ufw default deny incoming
-ufw default allow outgoing
-ufw allow "$sshPort/tcp"
-ufw allow 30000/tcp
-ufw allow 443/tcp
-ufw enable
-ufw status
-try sudo ufw status
+try su -l $systemUser -c 'ufw default deny incoming'
+try su -l $systemUser -c 'ufw default allow outgoing'
+try su -l $systemUser -c 'ufw allow "$sshPort/tcp"'
+try su -l $systemUser -c 'ufw allow 30000/tcp'
+try su -l $systemUser -c 'ufw allow 443/tcp'
+try su -l $systemUser -c 'ufw enable'
+try su -l $systemUser -c 'ufw status'
+try su -l $systemUser -c 'try sudo ufw status'
 shout "Successfully configured ports 30000, 443 & $sshPort.  Check you can login again before exiting the session"
 
 # 4. System Update
 shout "About to install system updates"
-apt update -y
-apt-get dist-upgrade
+try su -l $systemUser -c 'apt update -y'
+try su -l $systemUser -c 'apt-get dist-upgrade'
 shout "Successfully installed system updates"
 
 # 5 Shared Memory Read Only
 shout "Setting shared memory to read only"
-echo "
+try su -l $systemUser -c 'echo "
 none /run/shm tmpfs defaults,ro 0 0
-" >>/etc/fstab
-mount -a
+" >>/etc/fstab'
+try su -l $systemUser -c 'mount -a'
 
 ## 6 Install FIO (Test tool for IO)
 shout "Installing FIO (Test tool for IO)"
-apt install fio
+try su -l $systemUser -c 'apt install fio'
 shout "fio Install Complete. Run the following command to test 'fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=1G --readwrite=randrw --rwmixread=75'"
 
 ## 6 Install ZSTD (For uncompressing snapshots from https://snapshots.radix.live/)
 shout "Installing zstd (For uncompressing snapshots from https://snapshots.radix.live/)"
-apt install zstd
+try su -l $systemUser -c 'apt install zstd'
 mkdir /backup
 shout "zstd Install Complete and created /backup dir"
 
